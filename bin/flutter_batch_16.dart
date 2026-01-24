5a29c4e19f55921270a6c684adda2f84b9362fa3
moobin <moobinislam3@gmail.com>
2026-01-24 21:38:24 +0600

    only commit now

--- bin/flutter_batch_16.dart
+++ bin/flutter_batch_16.dart
@@ -1,5 +1,5 @@
-import 'package:flutter_batch_16/flutter_batch_16.dart' as flutter_batch_16;
-
-void main(List<String> arguments) {
-  print('Hello world: ${flutter_batch_16.calculate()}!');
-}
+// import 'package:flutter_batch_16/flutter_batch_16.dart' as flutter_batch_16;
+//
+// void main(List<String> arguments) {
+//   print('Hello world: ${flutter_batch_16.calculate()}!');
+// }
--- test/flutter_batch_16_test.dart
+++ test/flutter_batch_16_test.dart
@@ -1,8 +1,8 @@
-import 'package:flutter_batch_16/flutter_batch_16.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+// import 'package:flutter_batch_16/flutter_batch_16.dart';
+// import 'package:test/test.dart';
+//
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
