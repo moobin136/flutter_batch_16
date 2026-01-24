c46dd4de1255db8188eabcb0799a5ec2642644eb
moobin <moobinislam3@gmail.com>
2026-01-24 22:10:03 +0600

    commit

--- test/flutter_batch_16_test.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:flutter_batch_16/flutter_batch_16.dart';
-// import 'package:test/test.dart';
-//
-// void main() {
-//   test('calculate', () {
-//     expect(calculate(), 42);
-//   });
-// }
