753080ed36f34ca146ab248a1fa2f0bb21f64323
moobin136 <moobinislam3@gmail.com>
2026-04-15 20:34:20 +0600

    ready

--- lib/test_one.dart
+++ lib/test_one.dart
@@ -1,4 +1,10 @@
-// Assignment 1--------------------------
+// 1. Create a class Book with properties: title, author, and price.
+//
+//  ● Use a constructor to initialize these values.
+//
+//  ● Write a method discountedPrice(double discountPercent) that calculates and returns the price after discount.
+//
+//  ● Create at least two book objects, and print their details along with discounted prices.
 
 class Book {
   final String title;
@@ -20,46 +26,3 @@ class Book {
     print('Book Price : ${price.toStringAsFixed(0)}');
   }
 }
-
-//Assignment on module 8 (Exam Week -1)
-// You must solve the following problems using Object Oriented Programming concepts:
-//
-// 1. Create a class Book with properties: title, author, and price.
-//
-//  ● Use a constructor to initialize these values.
-//
-//  ● Write a method discountedPrice(double discountPercent) that calculates and returns the price after discount.
-//
-//  ● Create at least two book objects, and print their details along with discounted prices.
-//
-// 2. Create a base class Employee with properties: name, salary.
-//
-//  ● Create two subclasses:
-//
-//  ● Manager → adds property department
-//
-//  ● Developer → adds property programmingLanguage
-//
-//  ● Both subclasses should have a method to display their complete information.
-//
-//  ● Create one Manager and one Developer object, and display their details.
-//
-// 3. Create an abstract class Appliance with:
-//
-//  ● Abstract method turnOn().
-//
-//  ● Abstract method turnOff().
-//
-// Then create two subclasses:
-//
-//  ● Fan → implements the methods with custom messages (e.g., "Fan is now running").
-//
-//  ● Light → implements the methods with custom messages (e.g., "Light is switched on").
-//
-//  ● Create one Fan and one Light object, and call their methods.
-//
-// ✅ Submission Guidelines
-//
-// Submit the entire code as a single pdf.
-//
-// Note: You have to submit the assignment by following the guideline.
